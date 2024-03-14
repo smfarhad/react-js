9fb2b82d9e0018f3b37098c061aaad848e77d1fc
smfarhad <farhad1556@gmail.com>
2024-03-14 10:32:13 +0600

    react intro page

---
+++ src/components/Food.jsx
@@ -0,0 +1,13 @@
+export default function Food() {
+
+    const food1 = "Apple";
+    const food2 = "Banana";
+
+    return(
+        <ul>
+            <li>Apple</li>
+            <li>{food1}</li>
+            <li>{food2.toUpperCase()}</li>
+        </ul>
+    )
+}
---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Your website name </p>
+        </footer>
+    )
+}
