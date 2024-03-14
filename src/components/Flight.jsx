b7dd9af3fd0ea8b114bb39a37f08e8bd04a6b2c0
smfarhad <farhad1556@gmail.com>
2024-03-14 16:01:50 +0600

    Clear Code

--- src/components/Flight.jsx
+++ src/components/Flight.jsx
@@ -2,6 +2,7 @@
 //import flightImage from '../assets/images/1-200x200.jpg'
 import Button from './common/button/Button';
 import { PropTypes } from 'prop-types';
+import IsTicketable from './IsTicketable'
 Flight.propTypes = {
   flightNo: PropTypes.number.isRequired,
   imgSrc: PropTypes.string.isRequired,
@@ -28,7 +29,7 @@ export default function Flight({flightNo=0,imgSrc=defaultImgSrc,ticketAble=false
                 <p>Pellentesque vel nisl diam. Sed massa massa, placerat eget auctor at, pharetra at tortor. Curabitur commodo nulla et purus lacinia fermentum. In porta lectus at lacus eleifend efficitur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec consectetur, justo sit amet egestas pharetra, elit eros tincidunt magna, a vestibulum nulla mi id mi. Duis at aliquam lacus. Phasellus malesuada enim tempor gravida mollis.</p>
                 <p>Vestibulum sed nulla consequat, fringilla urna vel, convallis velit. Maecenas sem velit, iaculis ut arcu a, luctus ornare mauris. Nulla velit mauris, euismod eget lorem sed, lobortis interdum turpis. Morbi vel ex vel nisi commodo semper. Quisque placerat sit amet odio blandit porttitor. Nunc scelerisque molestie mi, cursus condimentum sapien pretium et. Sed imperdiet vestibulum viverra. Cras eu elit lacus. Cras iaculis velit nec arcu porta, vitae efficitur tortor ultrices.</p>
                 <div>
-                    <span className={`d-inline-flex mb-3 px-2 py-0 bg-${ticketAble?'success':'danger'}-subtle border border-${ticketAble?'success':'danger'}-subtle rounded-2`}> isTicketAble : {ticketAble?'Yes':'No'}</span>
+                   <IsTicketable isTrue={ticketAble}/>
                 </div>
                 <div>
                   <Button name="Book Now"/>
