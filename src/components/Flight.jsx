1c4f2c9b4f1f3586566e7bf3be17f3b338c1f2de
smfarhad <farhad1556@gmail.com>
2024-03-14 16:13:28 +0600

    Clear Code

--- src/components/Flight.jsx
+++ src/components/Flight.jsx
@@ -29,7 +29,7 @@ export default function Flight({flightNo=0,imgSrc=defaultImgSrc,ticketAble=false
                 <p>Pellentesque vel nisl diam. Sed massa massa, placerat eget auctor at, pharetra at tortor. Curabitur commodo nulla et purus lacinia fermentum. In porta lectus at lacus eleifend efficitur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec consectetur, justo sit amet egestas pharetra, elit eros tincidunt magna, a vestibulum nulla mi id mi. Duis at aliquam lacus. Phasellus malesuada enim tempor gravida mollis.</p>
                 <p>Vestibulum sed nulla consequat, fringilla urna vel, convallis velit. Maecenas sem velit, iaculis ut arcu a, luctus ornare mauris. Nulla velit mauris, euismod eget lorem sed, lobortis interdum turpis. Morbi vel ex vel nisi commodo semper. Quisque placerat sit amet odio blandit porttitor. Nunc scelerisque molestie mi, cursus condimentum sapien pretium et. Sed imperdiet vestibulum viverra. Cras eu elit lacus. Cras iaculis velit nec arcu porta, vitae efficitur tortor ultrices.</p>
                 <div>
-                   <IsTicketable isTrue={ticketAble}/>
+                  <IsTicketable isTrue={ticketAble}/>
                 </div>
                 <div>
                   <Button name="Book Now"/>
