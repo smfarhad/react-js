d9bf6167ac5d099e35c409d54b2f663963150609
smfarhad <farhad1556@gmail.com>
2024-03-17 16:55:09 +0600

    Event handling wp

--- src/components/Flight.jsx
+++ src/components/Flight.jsx
@@ -1,38 +1,48 @@
 // Static Image Call
 //import flightImage from '../assets/images/1-200x200.jpg'
-import Button from './common/button/Button';
-import { PropTypes } from 'prop-types';
-import IsTicketable from './IsTicketable'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+import Button from "./common/button/Button";
+import { PropTypes } from "prop-types";
+import IsTicketable from "./IsTicketable";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
 Flight.propTypes = {
-  flightData: PropTypes.object.isRequired
+  flightData: PropTypes.object.isRequired,
 };
 
-const planeFlight = '/src/assets/images/plane-flight.png';
-export default function Flight({flightData}){
-  const routes = flightData.legs; 
+const planeFlight = "/src/assets/images/plane-flight.png";
+export default function Flight({ flightData }) {
+  const routes = flightData.legs;
   const firstRoute = routes[0];
   const firstSegment = firstRoute.segmentDetails[0];
-  const lastSegment = firstRoute.segmentDetails[firstRoute.segmentDetails.length-1];
-  const origin = firstSegment.origin; 
-  const destination = lastSegment.destination; 
+  const lastSegment =
+    firstRoute.segmentDetails[firstRoute.segmentDetails.length - 1];
+  const origin = firstSegment.origin;
+  const destination = lastSegment.destination;
   const prices = flightData.priceBreakDownWithMarkup;
   const netTotalPrice = prices.totalFare;
   //const destination = lastSegment.destination;
-  console.log('s');
+  console.log("s");
   console.log(netTotalPrice.netTotalGrossFareAmount);
-  
-  console.log('e'); 
-  console.log(flightData); 
+
+  console.log("e");
+  console.log(flightData);
+  const handleClick = ()=>{
+    alert('Handle Event');
+  }
   return (
-    <div className='mb-3' data-flgiht={flightData.id}>
+    <div className="mb-3" data-flgiht={flightData.id}>
       <div className="card col-12 mb-12">
         <div className="card-header">
           <div className="row">
             <div className="col-12">
-               <p>{origin.dateTime}</p>
-               <p>{origin.airport} <span><img src={planeFlight} alt=">" /></span> {destination.airport}</p>
+              <p>{origin.dateTime}</p>
+              <p>
+                {origin.airport}{" "}
+                <span>
+                  <img src={planeFlight} alt=">" />
+                </span>{" "}
+                {destination.airport}
+              </p>
             </div>
           </div>
         </div>
@@ -40,22 +50,33 @@ export default function Flight({flightData}){
           <div className="row">
             <div className="col-9 left-section">
               <div>
-                <p>Pellentesque vel nisl diam. Sed massa massa, placerat fficitur.  conubia nostra, per inceptos himenaeos.    Duis at aliquam lacus. Phasellus malesuada enim tempor gravida mollis.</p>
-                <p>Vestibulum sed nulla consequat, fringilla urna vel,  lobortis interdum turpis.  Sed imperdiet vestibulum viverra.   vitae efficitur tortor ultrices.</p>
+                <p>
+                  Pellentesque vel nisl diam. Sed massa massa, placerat
+                  fficitur. conubia nostra, per inceptos himenaeos. Duis at
+                  aliquam lacus. Phasellus malesuada enim tempor gravida mollis.
+                </p>
+                <p>
+                  Vestibulum sed nulla consequat, fringilla urna vel, lobortis
+                  interdum turpis. Sed imperdiet vestibulum viverra. vitae
+                  efficitur tortor ultrices.
+                </p>
                 <div>
-                  <IsTicketable name='isTicketAble' isTrue={true}/> &nbsp;  
-                  <IsTicketable name='Refundable' isTrue={!flightData.nonRefundable}/>
+                  <IsTicketable name="isTicketAble" isTrue={true} /> &nbsp;
+                  <IsTicketable
+                    name="Refundable"
+                    isTrue={!flightData.nonRefundable}
+                  />
                 </div>
               </div>
             </div>
-            <div className="col-3 right-section">   
-              <div className='text-center right-section-ctn'>
-                  <div> BDT {netTotalPrice.netTotalFareAmount}</div>
-                  <div> BDT {netTotalPrice.netTotalGrossFareAmount}</div>
-                  <Button name="Book Now"/> &nbsp;
-                  <span>
-                    <FontAwesomeIcon icon={faQuestionCircle} />
-                  </span>
+            <div className="col-3 right-section">
+              <div className="text-center right-section-ctn">
+                <div> BDT {netTotalPrice.netTotalFareAmount}</div>
+                <div> BDT {netTotalPrice.netTotalGrossFareAmount}</div>
+                <Button name="Book Now" /> &nbsp;
+                <span onClick={handleClick} title="fareDetails">
+                  <FontAwesomeIcon icon={faQuestionCircle} />
+                </span>
               </div>
             </div>
           </div>
@@ -66,5 +87,5 @@ export default function Flight({flightData}){
 }
 
 Flight.defaultProps = {
-  flightData:{}
+  flightData: {},
 };
